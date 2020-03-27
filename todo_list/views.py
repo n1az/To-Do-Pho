0f3f66fa0b8d13afcb9ebfaa0f0d39956904b1d0
n1az <muhammadniazmorshed@gmail.com>
2020-03-27 16:59:11 -0700

    initial commit

---
+++ todo_list/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import List
+
+def home(request):
+	all_items = List.objects.all
+	return render(request, 'home.html', {'all_items': all_items})
+
+
+def about(request):
+	return render(request, 'about.html', {})
