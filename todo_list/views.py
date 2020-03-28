e7bbdb4ffbb8aef228b8f96df5b47bcd343aa087
n1az <muhammadniazmorshed@gmail.com>
2020-03-28 05:07:13 -0700

    Links Handled and Edit working

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,10 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
-	all_items = List.objects.all
-	return render(request, 'home.html', {'all_items': all_items})
+	if request.method == 'POST':
+		form = ListForm(request.POST or None)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request, ('Item Has Been Added To The List'))
+			return render(request, 'home.html', {'all_items': all_items})
+
+
+	else:
+		all_items = List.objects.all
+		return render(request, 'home.html', {'all_items': all_items})
 
 
 def about(request):
 	return render(request, 'about.html', {})
+
+
+def delete(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.delete()
+	messages.success(request, ('Item Has Been Deleted'))
+	return redirect('home')
+
+def cross_off(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = True
+	item.save()
+	return redirect('home')
+
+def uncross(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = False
+	item.save()
+	return redirect('home')
+
+def edit(request, list_id):
+	if request.method == 'POST':
+		item = List.objects.get(pk=list_id)
+		form = ListForm(request.POST or None, instance=item)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Item Has Been Edited'))
+			return redirect('home')
+
+	else:
+		item = List.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'item': item})
